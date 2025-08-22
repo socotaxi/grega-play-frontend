4e2513764a20f15f45975e77012b1b232b00a46b
Edhem <edhemrombhot@gmail.com>
2025-08-22 15:54:09 +0100

    Mise à jour Dashboard

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -135,7 +135,7 @@ const DashboardPage = () => {
 
         {events.length === 0 ? (
           <div className="bg-white shadow rounded-lg p-6 text-center">
-            <svg className="mx-auto h-12 w-12 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 24 24" stroke="currentColor">
+            <svg className="mx-auto h-12 w-12 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4" />
             </svg>
             <h3 className="mt-2 text-sm font-medium text-gray-900">Aucun événement</h3>
